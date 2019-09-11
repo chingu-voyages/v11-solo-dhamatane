0e72011995ad5314cfb2fbf211055d58f81fbb83
Hamatane Diallo <dhamatane@gmail.com>
2019-09-11 17:24:50 +0000

    Revert "create components"
    This reverts commit 2d3134b0441eeff9f1352dfc6531539f54b13989.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, {Component} from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import About from './components/About';
-import Service from './components/Service';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header />
-        {/* <Main />
-        <About />
-        <Service /> */}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
