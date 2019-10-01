183741dc05605218fd9dd317bdf5f30d91360ff7
Hamatane Diallo <dhamatane@gmail.com>
2019-10-01 09:59:39 +0000

    deploy app

---
+++ build/precache-manifest.bb7f408a891c35e945ddc1d4dc48f29d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffda3b93596403bfa6854e10454b3d60",
+    "url": "/v11-solo-dhamatane/index.html"
+  },
+  {
+    "revision": "c6c6c4dd65d241a7f71e",
+    "url": "/v11-solo-dhamatane/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ba30eb7579c41d05e583",
+    "url": "/v11-solo-dhamatane/static/js/2.3392cb3d.chunk.js"
+  },
+  {
+    "revision": "c6c6c4dd65d241a7f71e",
+    "url": "/v11-solo-dhamatane/static/js/main.d8405806.chunk.js"
+  },
+  {
+    "revision": "1d9854eea72ce3846fe0",
+    "url": "/v11-solo-dhamatane/static/js/runtime~main.fe899c36.js"
+  }
+]);
